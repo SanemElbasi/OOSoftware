987b6606200206b145c7464d68901cdfb2a9c832
SanemElbasi <sanem.elbasi@hotmail.com>
2015-07-28 20:59:27 +0300

    card modified

--- CardGame/src/Model/Card.java
+++ CardGame/src/Model/Card.java
@@ -1,5 +1,51 @@
 package Model;
 
-public class Card {
+import java.util.Observable;
 
+public class Card extends Observable{
+
+	private Suit cardSuit;
+	private int cardNumber;
+	private String specialty;
+//	private Image img;
+	
+	public Card(Suit cardSuit, int cardNumber){
+		this.cardSuit = cardSuit; 
+		this.cardNumber = cardNumber;
+		
+		if(cardNumber == 1 ){
+			specialty = "Skip 1";
+		}else if ( cardNumber == 7 ){
+			specialty = "Take 3";
+		}else if( cardNumber == 10 ){
+			specialty = "Change Rotation";
+		}else if( cardNumber == 11){
+			specialty = "Change Suit";
+		}else{
+			specialty = "None";
+		}
+		
+	}
+	
+	public Suit getSuit(){
+		return cardSuit;
+	}
+	
+	public void setSuit( Suit cardSuit){
+		this.cardSuit = cardSuit;
+		setChanged();
+		notifyObservers("Card Suit Changed!");
+	}
+	
+	public int getNumber(){
+		return cardNumber;
+	}
+	
+	public void setNumber(int cardNumber){
+		this.cardNumber = cardNumber;
+		setChanged();
+		notifyObservers("Card Number Changed!");
+	}
+	
+	
 }
